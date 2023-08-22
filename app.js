0f195c415d0266ef9b49e6a3996debe85e2348ef
umair kifayat <umairkifayat3@gmail.com>
2023-08-22 00:44:43 -0700

    Array

---
+++ app.js
@@ -0,0 +1,54 @@
+let names = 'Alice, Bob, Charlie, David, Eve';
+let arrays = names.split(' ');
+arrays[2]= "frank";
+// console.log(array);
+
+
+let nam = 'Alice, Bob, Charlie, David, Eve';
+let array = nam.split(' ');
+array[1] = "john";
+// console.log(array);
+
+
+
+let boys = ["Alice", "Bob", "Charlie", "David",  "Eve" ];
+
+boys.push("helen","grace")
+console.log(boys,arrays,array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let bikes = ["CD-70", "CG-125", "S-150", "S-110"]
+// bikes.splice(1, 1, "suzuki");
+// console.log(bikes);
+
+
+
+// let names = 'my name is Muhammad Abdullah khan and my age is 20';
+// let newArr = names.split(' ');
+// console.log(newArr)
+
+
+
+
+
+// let names = 'my name is Muhammad Abdullah khan and my age is 20';
+// let newArr = names.split(' ');
+// newArr[4] = "usman";
+// newArr.splice(6);
+// console.log(newArr);
+// let newStr = newArr.join(' ');
+// console.log(newStr);
+
+
+
